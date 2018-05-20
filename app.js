1f29cff8d6330a7ce9228b36e8d6ee6513674b92
ylkjcjq <691424873@qq.com>
2018-05-20 14:16:49 +0800

    a

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.post('/aubitex/notify/eth', ControllerNotify.notify);//数据监听
+app.post('/aubitex/notify/eth/btc/all', ControllerNotify.notify);//数据监听
 
 var port = process.env.PORT || 8066;
 app.listen(port);
