9c3d359f46bb4b86c9516fc87b3e53080cfd92f3
ylkjcjq <691424873@qq.com>
2018-05-21 13:19:25 +0800

    100mb

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const express = require("express"),
 const ControllerNotify = require("./api/controllers/notify_controller").ControllerNotify;
 
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit:'100mb'}));
+app.use(bodyParser.urlencoded({limit:'100mb',extended:true}));
+app.enable('trust proxy');//防止ip代理
 
 
 app.post('/aubitex/notify/eth/btc/all', ControllerNotify.notify);//数据监听
